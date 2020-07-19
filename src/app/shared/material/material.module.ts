5c48ff342fd9a64669cf70cb707e4e7747c048cd
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-07-19 12:13:34 -0500

    terimnado el dialog para iniciar secion y registro

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -9,6 +9,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -20,7 +23,10 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatSidenavModule,
     MatCardModule,
     MatTableModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     MatIconModule,
@@ -29,7 +35,10 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatSidenavModule,
     MatCardModule,
     MatTableModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
