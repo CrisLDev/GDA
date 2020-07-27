99b51549a78060fb6d981013c49cf00307c8c804
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-07-27 07:48:29 -0500

    Antes de la reestructuracion

--- src/app/pages/employe/employe.module.ts
+++ src/app/pages/employe/employe.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 
 // Modules
 import {EmployeComponent} from './employe.component';
-import {EmployeEditCreateComponent} from './components/employe-edit-create/employe-edit-create.component';
+import {EmployeEditCreateComponent} from '@pages/employe/employe-edit-create/employe-edit-create.component';
 import {EmployeRouting} from './employe.routing';
 import {SharedModule} from '@shared/shared.module';
 import {LayoutModule} from '@shared/components/layout/layout.module';
 import {EmployeService} from '@core/services/employe/employe.service';
 import {StoreModule} from '@ngrx/store';
 import * as fromEmploye from '@core/ngrx/reducers/employe.reducer';
+import { EmployesListComponent } from '@pages/employe/employes-list/employes-list.component';
 
 @NgModule({
-  declarations: [EmployeComponent, EmployeEditCreateComponent],
+  declarations: [EmployeComponent, EmployeEditCreateComponent,EmployesListComponent],
   imports: [
     CommonModule,
     EmployeRouting,
