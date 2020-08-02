d851b43c34d01fd6b3818d8df3c2621dafba023f
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-08-01 21:29:54 -0500

    Creando el servicio y la clase para maquinaria

---
+++ src/app/core/services/machinery.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Machinery} from '@shared/clases/Machinery/machinery';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MachineryService {
+
+  constructor() { }
+
+  
+}
---
+++ src/app/shared/clases/Machinery/Machinery.ts
@@ -0,0 +1,10 @@
+export class Machinery{
+    _id: string;
+    name: string;
+    brand: string;
+    weight: string;
+    status: string;
+    dateRegister: Date;
+    description: string;
+    image: string;
+}
