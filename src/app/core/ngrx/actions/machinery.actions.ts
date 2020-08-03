5d936c508e5b992dd7bf7c21ecc49fe5f4d272e4
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-08-03 15:43:31 -0500

    creado el action para crear y generado el reducer

---
+++ src/app/core/ngrx/actions/machinery.actions.ts
@@ -0,0 +1,34 @@
+import { createAction, props } from '@ngrx/store';
+import { create } from 'domain';
+import { Machinery } from '@app/shared/clases/Machinery/machinery';
+
+// Get all machineries
+export const getMachineries = createAction(
+  '[Machinery List Component] Load Machinerys'
+);
+
+export const getMachinerysSuccess = createAction(
+  '[Machinery] Load Machinerys Success',
+  props<{ data: any }>()
+);
+
+export const getMachinerysFailure = createAction(
+  '[Machinery] Load Machinerys Failure',
+  props<{ error: any }>()
+);
+
+// Create machinery
+export const createMachinery = createAction(
+  '[Machinery component] Create Machiney',
+  props<{machinery: Machinery}>()
+);
+
+export const createMachinerySuccess = createAction(
+  '[Create Machinery Effect Success] Create Machinery Success',
+  props<{machinery: Machinery}>()
+);
+
+export const createMachineryFailure = createAction(
+  '[Create Machinery Effect Failure] Create Machinery Failure',
+  props<{error: any}>()
+);
---
+++ src/app/core/ngrx/reducers/machinery.reducer.spec.ts
@@ -0,0 +1,13 @@
+import { reducer, initialState } from './machinery.reducer';
+
+describe('Machinery Reducer', () => {
+  describe('an unknown action', () => {
+    it('should return the previous state', () => {
+      const action = {} as any;
+
+      const result = reducer(initialState, action);
+
+      expect(result).toBe(initialState);
+    });
+  });
+});
